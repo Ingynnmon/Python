042a9cd469043e7a75afddb38603d92594bcb341
Ingynnmon <ingynnmon@ucsy.edu.mm>
2019-09-26 16:32:42 +0630

    test module

--- moduletest.py
+++ moduletest.py
@@ -1,9 +1,11 @@
 import mymodule
+import mary
 
 x = mymodule.student["code"]
 y = mymodule.student1["mother"]
 z = mymodule.student2["age"]
 
-print(x)
-print(y)
-print(z)
+print(x, ' ', y, ' ', z)
+
+for i in range(len(mary.a)):
+	print(i, mary.a[i])
