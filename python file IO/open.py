99c165e05d0689d7547b3bcf0457b642028cad2b
Ingynnmon <ingynnmon@ucsy.edu.mm>
2019-09-27 16:44:05 +0630

    output file name

--- python file IO/open.py
+++ python file IO/open.py
@@ -1,3 +1,7 @@
-with open('test.txt', 'r') as f:
-	f_text = f.readline()
-	print(f_text, end='')
+# with open('test.txt', 'r') as f:
+# 	f_text = f.readline()
+# 	print(f_text, end='')
+
+f = open('test.txt', 'r')
+print(f.name)
+f.close()
