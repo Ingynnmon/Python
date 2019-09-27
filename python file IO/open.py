4540ac790090785f50230565ea7500c5445f636f
Ingynnmon <ingynnmon@ucsy.edu.mm>
2019-09-27 21:43:20 +0630

    test

--- python file IO/open.py
+++ python file IO/open.py
@@ -3,11 +3,17 @@ with open('test.txt', 'r') as f:
 	scope_to_read =1000
 	f_text = f.read(scope_to_read)
 
-	while len(f_text) > 0:
-		print(f_text, end = '')
+	#f_text = f.readline()
+	#print(f_text,end='')
 
-# for n in f:
-# 	print(n, end='')
+	#while len(f_text) > 0:
+	#	print(f_text, end = '')
+
+	for n in f_text:
+		print(n,end = '')
+
+#for n in f:
+#	print(n, end='')
 
 # f = open('test.txt', 'r')
 # print(f.name)
